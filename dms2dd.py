600ce211cdf73764f4b2f61eaf49fb6f3c07194e
djfish1 <6416381+djfish1@users.noreply.github.com>
2019-08-26 21:25:12 -0700

    Degree conversion tools

---
+++ dms2dd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+""" Convert degrees/minutes/seconds to decimal degrees """
+import math
+import sys
+if (len(sys.argv) > 3):
+  degs = float(sys.argv[1])
+  mins = float(sys.argv[2])
+  secs = float(sys.argv[3])
+  absDegs = abs(degs)
+  absDecDeg = absDegs + mins / 60.0 + secs / 3600.0
+  print('{0:.6f}\n'.format(math.copysign(absDecDeg, degs)))
+else:
+  sys.stderr.write('Usage: dms2dd.py <degrees> <minutes> <seconds>\n')
